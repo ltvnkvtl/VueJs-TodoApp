c3c80b267b0a6ae4abc9791fc8685598c4aaddbe
ltvnkvtl <vital.litovk@gmail.com>
2020-08-29 19:58:50 +0300

    todo-app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>Todo application</h1>
+        <hr>
+
+        <router-view />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import TodoList from '@/components/TodoList';
+    import AddTodo from '@/components/AddTodo';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        data() {
+            return {
+                todos: [
+                    {id: 1, title: 'Купить 1', completed: false},
+                    {id: 2, title: 'Купить 2', completed: false},
+                    {id: 3, title: 'Купить 3', completed: false}
+                ]
+            }
+        },
+        mounted() {
+            fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
+                .then(response => response.json())
+                .then(json => this.todos = json);
+        },
+        methods: {
+            removeTodo(id) {
+                this.todos = this.todos.filter(todo => todo.id !== id);
+            },
+            addTodo(newTodo) {
+                this.todos.push(newTodo);
+            }
+        },
+        components: {
+            TodoList,
+            AddTodo
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dicta dolores et illo labore nisi
+            repudiandae tenetur velit voluptate voluptates.</p>
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
